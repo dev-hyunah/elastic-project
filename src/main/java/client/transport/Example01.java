55bfecbe941c30e7f3f7bc23711e336bff5c91a7
HYUNAH SEO <seoha9630@naver.com>
2022-02-22 10:46:25 +0900

    Transport Client 연결

---
+++ src/main/java/client/transport/Example01.java
@@ -0,0 +1,34 @@
+package client.transport;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+/**
+ * Transport 연결
+ *
+ * @author User
+ */
+public class Example01 {
+
+  @SuppressWarnings({"resource", "unchecked"})
+  public static void main(String[] args) throws UnknownHostException {
+
+    Settings settings = Settings.builder()
+                                .put("cluster.name", "javacafe-cluster")
+                                .put("client.transport.sniff", true)
+                                .build();
+
+    TransportClient client =
+        new PreBuiltTransportClient(settings)
+            .addTransportAddress(new TransportAddress(
+                InetAddress.getByName("127.0.0.1"), 9300));
+
+    client.close();
+  }
+}
