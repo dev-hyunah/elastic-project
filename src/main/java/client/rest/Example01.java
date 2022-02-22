d559cf4f4992f0ecae3e3f47c3381b7d1859ccbe
HYUNAH SEO <seoha9630@naver.com>
2022-02-22 16:09:46 +0900

    줄맞춤 수정

--- src/main/java/client/rest/Example01.java
+++ src/main/java/client/rest/Example01.java
@@ -14,60 +14,60 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 
 public class Example01 {
-    /**
-     * 인덱스 생성 및 매핑생성
-     * */
-    public static void main(String[] args) throws IOException {
-        
-    	RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("127.0.0.1", 9200, "http")));
+  /**
+   * 인덱스 생성 및 매핑생성
+   */
+  public static void main(String[] args) throws IOException {
 
-    	// Index명
-        String INDEX_NAME = "movie_rest";
-        
-        // 타입명
-        String TYPE_NAME = "_doc";
+    RestHighLevelClient client = new RestHighLevelClient(
+        RestClient.builder(
+            new HttpHost("127.0.0.1", 9200, "http")));
 
-        // 매핑정보
-        XContentBuilder indexBuilder = jsonBuilder()
-        		.startObject()
-	        		.startObject(TYPE_NAME)
-		                .startObject("properties")
-			                .startObject("movieCd")
-				                .field("type","keyword")
-				                .field("store","true")
-				                .field("index_options","docs")
-			                .endObject()
-			                .startObject("movieNm")
-				                .field("type","text")
-				                .field("store","true")
-				                .field("index_options","docs")
-			                .endObject()
-			                .startObject("movieNmEn")
-				                .field("type","text")
-				                .field("store","true")
-				                .field("index_options","docs")
-			                .endObject()
-		                .endObject()
-	                .endObject()
-                .endObject();
+    // Index명
+    String INDEX_NAME = "movie_rest";
 
-        // 매핑 설정
-        CreateIndexRequest request = new CreateIndexRequest(INDEX_NAME);
-        request.mapping(TYPE_NAME,indexBuilder);
+    // 타입명
+    String TYPE_NAME = "_doc";
 
-        // Alias 설정
-        String ALIAS_NAME = "moive_auto_alias";
-        request.alias(new Alias(ALIAS_NAME));
+    // 매핑정보
+    XContentBuilder indexBuilder = jsonBuilder()
+        .startObject()
+        .startObject(TYPE_NAME)
+        .startObject("properties")
+        .startObject("movieCd")
+        .field("type", "keyword")
+        .field("store", "true")
+        .field("index_options", "docs")
+        .endObject()
+        .startObject("movieNm")
+        .field("type", "text")
+        .field("store", "true")
+        .field("index_options", "docs")
+        .endObject()
+        .startObject("movieNmEn")
+        .field("type", "text")
+        .field("store", "true")
+        .field("index_options", "docs")
+        .endObject()
+        .endObject()
+        .endObject()
+        .endObject();
 
-        // 인덱스 생성
-        CreateIndexResponse createIndexResponse = 
-        		client.indices().create(request, RequestOptions.DEFAULT);
-        
-        
-        boolean acknowledged = createIndexResponse.isAcknowledged();
+    // 매핑 설정
+    CreateIndexRequest request = new CreateIndexRequest(INDEX_NAME);
+    request.mapping(TYPE_NAME, indexBuilder);
 
-        client.close();
-    }
+    // Alias 설정
+    String ALIAS_NAME = "moive_auto_alias";
+    request.alias(new Alias(ALIAS_NAME));
+
+    // 인덱스 생성
+    CreateIndexResponse createIndexResponse =
+        client.indices().create(request, RequestOptions.DEFAULT);
+
+
+    boolean acknowledged = createIndexResponse.isAcknowledged();
+
+    client.close();
+  }
 }
